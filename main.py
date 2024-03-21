82eb92dca1035e85f02e0e77dc7809cf0d98b116
edmond mulera <edmondmulera55@gmail.com>
2024-03-21 11:46:47 +0300

    connect with the smtp

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from function import scrape, extract, send_email, store, read
-
+import time
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email(message="Hey Ed new event was found")
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey Ed new event was found")
+        time.sleep(3)
