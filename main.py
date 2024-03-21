1c9764cd69fe98a3553d405a55e3e2160181c6f6
edmond mulera <edmondmulera55@gmail.com>
2024-03-21 09:02:55 +0300

    create a function that extract the information from the website

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import selectorlib
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
-
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 def scrape(url):
     """scrape the page source from URL"""
