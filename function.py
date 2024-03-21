f39747e7fff013f082bfd369a486b55ac51f5294
edmond mulera <edmondmulera55@gmail.com>
2024-03-21 10:14:39 +0300

    extract function extracted the information needed

---
+++ function.py
@@ -0,0 +1,17 @@
+import requests
+import selectorlib
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(url):
+    """scrape the page source from URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-import requests
-import selectorlib
+from function import scrape, extract
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-def scrapee(url):
-    """scrape the page source from URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
-
-
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
 
 
 if __name__ == "__main__":
-    scraped = scrapee(URL)
+    scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
